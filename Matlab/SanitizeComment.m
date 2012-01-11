4b1b95e82e25bc769f1b609119efa4d5c39daddf
Mark <faridani@gmail.com>
2012-01-10 23:53:39 -0800

    Cleaned up SanitizeComment.m

--- Matlab/SanitizeComment.m
+++ Matlab/SanitizeComment.m
@@ -2,43 +2,14 @@ function out1 = SanitizeComment(in1)
     % clears a string from puctuations
 	
 	
-    %myoutstring =regexprep(in1, '\,', ' ');
+   
     myoutstring =regexprep(char(in1), '[^\w\s]', ' ');
-	%myoutstring =regexprep(myoutstring, char(13), 'goldfish');
+	
+	% the following part takes care of \n in MATLAB
+	% MATLAB's regex has a bug and could not handle these two cases
+	
 	myoutstring(myoutstring==13)=[];
 	myoutstring(myoutstring==10)=[];
-%     
-%     
-%     myoutstring =regexprep(myoutstring, '\''', ' ');
-%     myoutstring =regexprep(myoutstring, '\-', ' ');
-%     myoutstring =regexprep(myoutstring, '\.', ' ');
-%     myoutstring =regexprep(myoutstring, '\"', ' ');
-%     myoutstring =regexprep(myoutstring, '(', ' ');
-%     myoutstring =regexprep(myoutstring, ')', ' ');
-%     myoutstring =regexprep(myoutstring, '\?', ' ');
-%     myoutstring =regexprep(myoutstring, '\*', ' ');
-%     myoutstring =regexprep(myoutstring, '\/', ' ');
-%     myoutstring =regexprep(myoutstring, '\:', ' ');
-%     myoutstring =regexprep(myoutstring, '\!', ' ');
-%     myoutstring =regexprep(myoutstring, '\&', ' ');
-%     myoutstring =regexprep(myoutstring, '\$', ' ');
-%     myoutstring =regexprep(myoutstring, '1', ' ');
-%     myoutstring =regexprep(myoutstring, '2', ' ');
-%     myoutstring =regexprep(myoutstring, '3', ' ');
-%     myoutstring =regexprep(myoutstring, '4', ' ');
-%     myoutstring =regexprep(myoutstring, '5', ' ');
-%     myoutstring =regexprep(myoutstring, '6', ' ');
-%     myoutstring =regexprep(myoutstring, '7', ' ');
-%     myoutstring =regexprep(myoutstring, '8', ' ');
-%     myoutstring =regexprep(myoutstring, '9', ' ');
-%     myoutstring =regexprep(myoutstring, '0', ' ');
-%     myoutstring =regexprep(myoutstring, '\#', ' ');
-%     myoutstring =regexprep(myoutstring, '\%', ' ');
-%     myoutstring =regexprep(myoutstring, '\^', ' ');
-%    
-    
-    
-    myoutstring =regexprep(myoutstring, 'Please enter your response here', ' ');
     
     
     out1 =  myoutstring;
